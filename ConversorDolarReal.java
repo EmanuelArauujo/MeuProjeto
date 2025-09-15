7761810691673492286b28296aa601414e54ede6
EmanuelArauujo <carlosemanuelpintodearaujo@gmail.com>
2025-09-15 19:32:10 -0300

    Update ConversorDolarReal.java

--- ConversorDolarReal.java
+++ ConversorDolarReal.java
@@ -1,1 +1,28 @@
+/*********************************************************************
+  * 2) Faça um programa que: 
+  * - Leia a cotação do dólar
+  * - Leia um valor em dólares
+  * - Converta esse valor para real
+  * - Mostre o resultado
+  *
+  *********************************************************************/
+import java.util.Scanner;
 
+public class ConversorDolarReal {
+  Scanner entrada = new Scanner(System.in);
+  double cotacao_dolar, valor_dolar, valor_real;
+
+  System.out.print("Conversor de dólar em real\n\n");
+
+  System.out.print("Digite a cotação do dólar: ");
+  cotacao_dolar = entrada.nextDouble();
+
+  valor_real = cotacao_dolar * valor_dolar;
+
+  System.out.print("O valor em reais é: "+ valor_real+ "\n");
+
+  }
+
+}
+
+  
