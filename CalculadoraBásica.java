02e942b2f95f47031c672a47658f6e4c2563fe5b
EmanuelArauujo <carlosemanuelpintodearaujo@gmail.com>
2025-10-24 19:09:17 -0300

    Update CalculadoraBásica.java

--- CalculadoraBásica.java
+++ CalculadoraBásica.java
@@ -1,1 +1,25 @@
+package Calculadora;
 
+import Java.util.Scanner;
+
+public class Calculadora() {
+
+  public static void main (String[] args) {
+    Scanner.sc = new Scanner(System.in);
+
+      System.out.print("Digite o primeiro número inteiro: ");
+      double a = Scanner.nextDouble();
+
+      System.ou.print("Digite o segundo número inteiro: ");
+      double b = Scanner.nextDouble();
+
+      System.out.print("O resultado da soma é: " + (a + b));
+      System.out.print("O resultado da subtração é: " + (a - b));
+      System.out.print("O resultado da multiplicação é: " + (a * b));
+      System.out.print("O resultado da divisão é: " + (a / b));
+    
+
+
+  }
+
+}
